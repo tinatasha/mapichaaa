a943585b9e75a4207f02f15336e815b09f1952c3
tinatasha <tashambiti@gmail.com>
2019-10-09 05:39:17 +0300

    deploy to heroku

--- mapichaaa/settings.py
+++ mapichaaa/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 
 db_from_env = dj_database_url.config()
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
