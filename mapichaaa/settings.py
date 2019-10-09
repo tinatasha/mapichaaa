015a611763f4970981ca308dad6fe833e75602d4
tinatasha <tashambiti@gmail.com>
2019-10-09 05:37:45 +0300

    deploy to heroku

--- mapichaaa/settings.py
+++ mapichaaa/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj-database-url
+import dj_database_url
 
 db_from_env = dj_database_url.config()
 DATABASES[‘default’].update(db_from_env)
